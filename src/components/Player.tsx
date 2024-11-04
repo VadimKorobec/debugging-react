f6a38f6592aad327464e16a44561ca03c314c66c
Vadim <_s_e_t__@ukr.net>
2024-11-04 18:00:39 +0200

    add some code

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState<string>('');
+  const [submitted, setSubmitted] = useState<boolean>(false); 
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const enteredName = event.target.value.trim();
+    setEnteredPlayerName(enteredName);
+  };
+
+  const handleClick = () => {
+    console.log(enteredPlayerName);
+    setSubmitted(true);
+  };
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" value={enteredPlayerName} onChange={handleChange} />
+        <button disabled={!enteredPlayerName} onClick={handleClick}>
+          Set Name
+        </button>
       </p>
     </section>
   );
